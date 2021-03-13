81ed119ad996d93482d01509e65e174fd376cd1b
Elvis Vidal da Silva <elvisvidal@Elviss-MacBook-Pro.local>
2021-03-13 20:28:28 +0000

    Find product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,10 @@
 const Product = require('../modules/productModule');
 
+/**
+ * Gets all produts
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ */
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
@@ -11,6 +16,30 @@ async function getProducts(req, res) {
   }
 }
 
+/**
+ * Get a produt by ID
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @param {Interger} id Product id
+ */
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { 'Contet-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Product not found' }));
+    } else {
+      res.writeHead(200, { 'Contet-Type': 'application/json' });
+      res.end(JSON.stringify(product));
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 module.exports = {
-  getProducts
+  getProducts,
+  getProduct
 }
